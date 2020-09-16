f0f3574482cec92fe4d840a26d949012544e8400
Sudarshan Ragunathan <sudarshan.ragunathan@barrowneuro.org>
2020-09-16 01:01:36 -0700

    New src and test files as examples

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name='src',
+        version = '1.0.0', 
+        install_requires = ['pytest'], 
+        packages = find_packages()
+        )
