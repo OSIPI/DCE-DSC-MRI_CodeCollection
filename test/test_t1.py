7d5f8dd1a30d77f926716a8a9750cafdc0301840
Zaki A <notZaki@users.noreply.github.com>
2020-10-18 16:58:55 -0500

    Modify failing tests
    - vfa_2point returns matrix instead of a vector. The returned value also have to be rounded
    - the exception was failing because one of the cases didn't contain invalid values, so the exception did not get raised

--- test/test_t1.py
+++ test/test_t1.py
@@ -23,20 +23,20 @@ def test_fit_vfa_linear(S_t, fa_array, tr_S, T1_est, S0):
 @pytest.mark.parametrize('S_t, fa_array, tr_S, T1_est, S0', [(np.array([48.86,58.95,69.14,63.22]),np.array([20,15,10,5]),0.01,1.,1000.)])
 def test_fit_vfa_2_point(S_t,fa_array, tr_S, T1_est, S0):
     fa_array_rad = fa_array * pi/180.
-    np.testing.assert_array_almost_equal(fit_vfa_2_point(S_t,fa_array_rad,tr_S),[S0,T1_est],decimal=2)
+    np.testing.assert_array_almost_equal(np.round(fit_vfa_2_point(S_t,fa_array_rad,tr_S)),[[S0],[T1_est]],decimal=2)
 
 # Test for valid input values :
 """
 If invalid flip angles are used i.e. <=0, then raise an exception
 """
-@pytest.mark.parametrize('fa_array',[(np.array([10,8,6,4])),
+@pytest.mark.parametrize('fa_array',[(np.array([-10,8,6,4])),
                                       (np.array([0,2,4,6])),
                                       (np.array([20,10,0,-5]))])
 def test_negative_favals(fa_array):
     with pytest.raises(ValueError) as exc_info:
         if fa_array.all() == False:
             raise ValueError("Invalid flip angle provided")
-    assert exc_info.type is ValueError
+            assert exc_info.type is ValueError
     
 
 # Test for valid input data types:
