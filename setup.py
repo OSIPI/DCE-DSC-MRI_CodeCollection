c9a255d74129d06b47633bc7f85bc0dcb102dcdc
Sudarshan Ragunathan <schudds@users.noreply.github.com>
2020-10-24 22:47:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
         name='src',
         version = '1.0.0', 
-        install_requires = ['pytest','numpy','scipy'],
+        install_requires = ['pytest','numpy','scipy','lmfit'],
         packages = find_packages()
         )
