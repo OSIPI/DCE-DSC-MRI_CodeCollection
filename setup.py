61012b6bba3892f13d485ecc87513e0259c262a0
mjt320 <mjt320@googlemail.com>
2021-03-11 13:41:50 +0000

    install pandas

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
         name='src',
         version = '1.0.0', 
-        install_requires = ['pytest','numpy','scipy','lmfit'],
+        install_requires = ['pytest','numpy','scipy','lmfit','pandas'],
         packages = find_packages()
         )
