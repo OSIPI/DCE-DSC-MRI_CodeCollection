9410c3781699c5a6946c55aabbcf1a74266cbcaf
mjt320 <mjt320@googlemail.com>
2021-05-26 17:26:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
         name='src',
         version = '1.0.0', 
-        install_requires = ['pytest','numpy','scipy','lmfit','pandas'],
+        install_requires = ['pytest','numpy','scipy','lmfit','pandas','joblib'],
         packages = find_packages()
         )
