f0524308bc708a17cf8d832508bf971620a8eb2c
Lucy Kershaw <lucyekershaw@gmail.com>
2021-08-30 10:01:26 +0100

    Added testing for SI to concentration functions

---
+++ test/SI_to_Conc/SI2Conc_data.py
@@ -0,0 +1,57 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+# Functions to return formatted test data for testing SI to concentration code
+
+def SI2Conc_data():
+    """
+    Import signal intensity data for testing converison to concentration
+    
+    Data summary: Signal intensity curves derived from 1 patient 
+    Patient(s): Randomly selected patient DCE-MRI of uterus 
+    Source: University of Edinburgh, Mechanistic substudy of UCON https://www.birmingham.ac.uk/research/bctu/trials/womens/ucon/ucon-home.aspx used with permission.
+    Detailed info: Each entry corresponds to signal intensity curve from voxels in uterus, or aorta.  They were converted to concentration
+    using code from then University of Edinburgh but with various flip angles, baseline T1 values etc rather than the actual values used in the 
+    acquisition, to test a wider range of possibilites.
+
+    Data file lines consist of; label, flip angle, TR, baseline T1, number of baseline points before contrast, r1, signal, concentration
+
+    Tolerance: 0.001 + 0.001 (relative) - this is just maths so it should be very similar for the different implementations
+   
+    Returns
+    -------
+    pars : list of tuples
+        Input for pytest.mark.parametrize
+        Each tuple contains a set of parameters corresponding to 1 test case
+    """    
+    filename = os.path.join(os.path.dirname(__file__), 'data', 'SI2Conc_data.csv')    
+    # read from CSV to pandas
+    converters = {
+        's': lambda x:np.fromstring(x, dtype=float, sep=' '),
+        'conc': lambda x:np.fromstring(x, dtype=float, sep=' '),
+        }              
+    df = pd.read_csv(filename, converters = converters)
+    
+    # convert to lists
+    label = df['label'].tolist() # label describing entry
+    fa = df['FA'].tolist() # degrees
+    tr = df['TR'].tolist() # s
+    T1base = df['T1base'].tolist() # s
+    BLpts = df['numbaselinepts'].tolist()
+    r1 = df['r1'].tolist()
+    s_array = df['s'].tolist()
+    conc_array = df['conc'].tolist()
+    
+    # set the tolerance to use for this dataset
+    a_tol = [0.001] * len(s_array)
+    r_tol = [0.001] * len(s_array)
+    
+    # convert to list of tuples (input for pytest.mark.parametrize)
+    pars = list(zip(label, fa, tr, T1base, BLpts, r1, s_array, conc_array, a_tol, r_tol))
+    
+    return pars
+
+
+
---
+++ test/SI_to_Conc/test_SI2Conc_OG_MO_AUMC_ICR_RMH.py
@@ -0,0 +1,36 @@
+import pytest
+import numpy as np
+
+from ..helpers import osipi_parametrize
+from . import SI2Conc_data
+from src.original.OG_MO_AUMC_ICR_RMH.ExtendedTofts.DCE import dce_to_r1eff
+from src.original.OG_MO_AUMC_ICR_RMH.ExtendedTofts.DCE import r1eff_to_conc
+
+
+
+# All tests will use the same arguments and same data...
+arg_names = 'label', 'fa', 'tr', 'T1base', 'BLpts', 'r1', 's_array', 'conc_array', 'a_tol', 'r_tol'
+test_data = SI2Conc_data.SI2Conc_data()
+
+
+# Use the test data to generate a parametrize decorator. This causes the following
+# test to be run for every test case listed in test_data...
+@osipi_parametrize(arg_names, test_data, xf_labels = [])
+def test_OG_MO_AUMC_ICR_RMH_dce_to_r1eff(label, fa, tr, T1base, BLpts, r1, s_array, conc_array, a_tol, r_tol):
+
+    ##Prepare input data
+    #Convert fa to radians
+    fa_rad=fa * np.pi/180.
+    #This function uses a value for S0, which would be the mean of the s_array from 1 to BLpts.
+    # It's from 1 rather than 0 because the code used to do the original conversion skip sthe first point of the SI curve
+    s0=np.mean(s_array[1:BLpts])
+    
+    # run test
+    #The code uses two functions to get from SI to conc
+    r1_curve = dce_to_r1eff(s_array, [s0], 1/T1base, tr, fa_rad)
+    #I don't really understand the input array shapes for this function but we end up with 150 versions of the curve. Use one!
+    conc_curve = r1eff_to_conc(r1_curve[:,0], 1/T1base, r1)
+
+
+    np.testing.assert_allclose( [conc_curve], [conc_array], rtol=r_tol, atol=a_tol )
+
