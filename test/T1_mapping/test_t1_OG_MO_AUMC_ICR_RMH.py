c0ca1e01f93d381dc9af08c7f4927cbcdbd187e5
Petra van Houdt <p.v.houdt@nki.nl>
2021-09-22 17:39:11 +0200

    added output results for ST and OG contributions

--- test/T1_mapping/test_t1_OG_MO_AUMC_ICR_RMH.py
+++ test/T1_mapping/test_t1_OG_MO_AUMC_ICR_RMH.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from ..helpers import osipi_parametrize
+from ..helpers import osipi_parametrize, log_init, log_results
 from . import t1_data
 from src.original.OG_MO_AUMC_ICR_RMH.ExtendedTofts.DCE import R1_two_fas
 
@@ -13,6 +13,14 @@ test_data = (
     t1_data.t1_quiba_data() +
     t1_data.t1_prostate_data()
     )
+
+filename_prefix = ''
+
+def setup_module(module):
+    # initialize the logfiles
+    global filename_prefix # we want to change the global variable
+    filename_prefix = 'TestResults_T1mapping'
+    log_init(filename_prefix, '_test_OG_MO_AUMC_ICR_RHM_t1_VFA_2fa', ['label', 'r1_ref', 'r1_measured'])
 
 
 # Use the test data to generate a parametrize decorator. This causes the following
@@ -31,5 +39,6 @@ def testOG_MO_AUMC_ICR_RMH_t1_VFA_2fa(label, fa_array, tr_array, s_array, r1_ref
     fa_array_rad = fa_array[[0, -1]] * np.pi/180. # use first and last FA only
     
     # run test (2 flip angle)
-    r1_2fa_meas = R1_two_fas(s_array_trimmed,fa_array_rad,tr)[0]
-    np.testing.assert_allclose( [r1_2fa_meas], [r1_ref], rtol=r_tol, atol=a_tol )
+    r1_2fa_meas = R1_two_fas(s_array_trimmed, fa_array_rad,tr)[0]
+    np.testing.assert_allclose([r1_2fa_meas], [r1_ref], rtol=r_tol, atol=a_tol)
+    log_results(filename_prefix, '_test_OG_MO_AUMC_ICR_RHM_t1_VFA_2fa', [label, r1_ref, r1_2fa_meas])
