6eac589892ede3b49170860f1797e7e6130be244
Petra van Houdt <p.v.houdt@nki.nl>
2021-09-22 17:29:19 +0200

    added saving output for McGill_Can tests

--- test/T1_mapping/test_t1_McGill_Can.py
+++ test/T1_mapping/test_t1_McGill_Can.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from ..helpers import osipi_parametrize
+from ..helpers import osipi_parametrize, log_init, log_results
 from . import t1_data
 from src.original.McGill_Can.vfa import despot, novifast
 
@@ -14,6 +14,14 @@ test_data = (
     t1_data.t1_prostate_data()
     )
 
+filename_prefix = ''
+
+def setup_module(module):
+    # initialize the logfiles
+    global filename_prefix # we want to change the global variable
+    filename_prefix = 'TestResults_T1mapping'
+    log_init(filename_prefix, '_test_mcgill_t1_novifast', ['label', 'r1_ref', 'r1_measured'])
+    log_init(filename_prefix, '_test_mcgill_t1_VFA_lin', ['label', 'r1_ref', 'r1_measured'])
 
 # Use the test data to generate a parametrize decorator. This causes the following
 # test to be run for every test case listed in test_data...
@@ -29,6 +37,7 @@ def test_mcgill_t1_novifast(label, fa_array, tr_array, s_array, r1_ref, s0_ref, 
     [s0_nonlin_meas, t1_nonlin_meas] = novifast(s_array,fa_array_rad,tr)
     r1_nonlin_meas = 1./t1_nonlin_meas[0]  
     np.testing.assert_allclose( [r1_nonlin_meas], [r1_ref], rtol=r_tol, atol=a_tol )
+    log_results(filename_prefix, '_test_mcgill_t1_novifast', [label, r1_ref, r1_nonlin_meas])
 
 
 # In the following test, we specify 1 case that is expected to fail...
@@ -45,3 +54,4 @@ def test_mcgill_t1_VFA_lin(label, fa_array, tr_array, s_array, r1_ref, s0_ref, a
     [s0_lin_meas, t1_lin_meas] = despot(s_array,fa_array_rad,tr)
     r1_lin_meas = 1./t1_lin_meas    
     np.testing.assert_allclose( [r1_lin_meas], [r1_ref], rtol=r_tol, atol=a_tol )
+    log_results(filename_prefix, '_test_mcgill_t1_VFA_lin', [label, r1_ref, r1_lin_meas])
