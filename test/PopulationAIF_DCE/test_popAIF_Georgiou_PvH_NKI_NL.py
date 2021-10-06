cb6ffd97051e9c429c5e878fd0c855dde91c4480
Petra van Houdt <p.v.houdt@nki.nl>
2021-10-06 19:33:50 +0200

    added and modified test function for Georgiou AIF

--- test/PopulationAIF_DCE/test_popAIF_Georgiou_PvH_NKI_NL.py
+++ test/PopulationAIF_DCE/test_popAIF_Georgiou_PvH_NKI_NL.py
@@ -1,49 +1,27 @@
 import os
 import pytest
 import numpy as np
-import math
-import matplotlib.pyplot as pyplot
-
-from src.original.PvH_NKI_NL.AIF.PopulationAIF import ParkerAIF, GeorgiouAIF
-#from PopulationAIF import ParkerAIF, GeorgiouAIF
+from test.helpers import osipi_parametrize
+from src.original.PvH_NKI_NL.AIF.PopulationAIF import GeorgiouAIF
+from . import popAIF_Georgiou_data
 
 
-dt = 1 #temp resolution in s
-endt = 280 # end time in s
-notimepoints = endt/dt
-time = np.arange(0,endt+dt, dt)
-
-AIF_G=GeorgiouAIF(time)
-pyplot.figure(1)    
-pyplot.plot(time.tolist(), AIF_G.tolist(), 'r')
-filename = os.path.join(os.path.dirname(__file__), 'data', 'GeorgiouAIF_MRM2018.txt')  
-timeaif, aifdata = np.loadtxt(filename, delimiter='\t', unpack=True)
-timeaif=timeaif*60  # convert time to seconds
-
-# interpolate to a given time series
-last_timeaif = timeaif[len(timeaif)-1]
-last_time = time[len(time)-1]
-if last_time <= last_timeaif:
-        # interpolate to the right time series
-    GeorgiouAIF_ref = np.interp(time, timeaif, aifdata)
-
-pyplot.plot(time.tolist(), GeorgiouAIF_ref.tolist(), 'b')
-
-# set the tolerance to use for this dataset
+# All tests will use the same arguments and same data...
+arg_names = 'label, time, Cb_ref_values, r_tol, a_tol'
+test_data = popAIF_Georgiou_data.GeorgiouAIF_refdata()
+#test_data = (
+#    popAIF_Georgiou_data.GeorgiouAIF_refdata()
+#    )
 
 
-#np.testing.assert_allclose( [AIF_G], [GeorgiouAIF_ref], rtol=a_tol, atol=r_tol,err_msg='difference outside tolerance', verbose=True)
+# Use the test data to generate a parametrize decorator. This causes the following
+# test to be run for every test case listed in test_data...
+@osipi_parametrize(arg_names, test_data, xf_labels=[])
+def test_Georgiou_AIF_PvH_NKI_NL(label, time, cb_ref_values, a_tol, r_tol):
 
-dt = 1 #temp resolution in s
-endt = 280 # end time in s
-notimepoints = endt/dt
-time_array= np.arange(0,endt+dt, dt)
-
-
-def test_Georgiou_AIF():#(time_array, ref_array, a_tol, r_tol):
-    a_tol = 0.05
-    r_tol = 0.05
-    
-    AIF_G=GeorgiouAIF(time_array)       
     # prepare input data
-    np.testing.assert_allclose([AIF_G], [GeorgiouAIF_ref], rtol=r_tol, atol=a_tol) 
+    # convert time to seconds
+    time = time*60
+    AIF_G = GeorgiouAIF(time)
+
+    np.testing.assert_allclose([AIF_G], [cb_ref_values], rtol=r_tol, atol=a_tol)
