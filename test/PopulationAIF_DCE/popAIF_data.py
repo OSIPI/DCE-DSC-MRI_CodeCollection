b6eb6f1b02a2db9be5a80ddc7bf82e7e601cfc95
Petra van Houdt <p.v.houdt@nki.nl>
2021-10-07 11:49:28 +0200

    added tests for ST_SydneyAus contribution + added delay test data for AIF

--- test/PopulationAIF_DCE/popAIF_data.py
+++ test/PopulationAIF_DCE/popAIF_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+import math
 
 def GeorgiouAIF_refdata():
     """
@@ -66,9 +67,9 @@ def ParkerAIF_refdata():
     These parameters in combination with equation 1 were used to create different versions of the ParkerAIF.
     The original data had a temp resolution of 4.97 s and tot acquisition time of 372.75 s. This data was labeled as 'Original_AIF'
     The other reference entries include AIF values with varying temporal resolutions and varying total acquisition times.
+    Data without a delay or pre-contrast value are assumed. Those are included in a separate testset
 
-    not included in test data yet:
-    - bolus arrival time variations
+    Bolus arrival time variations are included in ParkerAIF_refdata_delay
 
     References: Parker et al. MRM 2006, doi: 10.1002/mrm.21066
 
@@ -94,9 +95,10 @@ def ParkerAIF_refdata():
     label = []
     time = []
     cb_ref_values = []
+    delay = []
 
     label.append('original_AIF')
-    time_original = np.arange(0, 5, 4.97/60)
+    time_original = np.arange(0, 5, 4.97/60) # in min
     gaussian1 = A1 / (sigma1 * np.sqrt(2 * np.pi)) * np.exp(-np.square(time_original - T1) / (2 * np.square(sigma1)))
     gaussian2 = A2 / (sigma2 * np.sqrt(2 * np.pi)) * np.exp(-np.square(time_original - T2) / (2 * np.square(sigma2)))
     modSigm = (alpha * np.exp(-beta * time_original)) / (1 + np.exp(-s * (time_original - tau)))
@@ -104,6 +106,7 @@ def ParkerAIF_refdata():
     cb = np.add(cb, modSigm)
     time.append(time_original)
     cb_ref_values.append(cb)
+    delay.append(0)  # assume no delay
 
     # similar for a range of temporal resolutions
     range_dt = np.array([0.5, 1, 2, 2.5, 5, 7.5])  # range of realistic temporal resolutions
@@ -119,6 +122,7 @@ def ParkerAIF_refdata():
         cb = np.add(cb, modSigm)
         time.append(time_int)
         cb_ref_values.append(cb)
+        delay.append(0)  # assume no delay
 
     # similar for different acquisition times
     range_endt = np.array([3, 5, 7, 10])  # range of total acquisition time
@@ -134,12 +138,80 @@ def ParkerAIF_refdata():
         cb = np.add(cb, modSigm)
         time.append(time_int)
         cb_ref_values.append(cb)
+        delay.append(0)  # assume no delay
 
     # set the tolerance to use for this dataset
     a_tol = [0.05] * len(label)
     r_tol = [0.05] * len(label)
 
     # convert to list of tuples (input for pytest.mark.parametrize)
-    pars = list(zip(label, time, cb_ref_values, a_tol, r_tol))
+    pars = list(zip(label, time, cb_ref_values, delay, a_tol, r_tol))
+
+    return pars
+
+
+def ParkerAIF_refdata_delay():
+    """
+    This function creates the test data to test implementations of the Parker AIF including a delay or precontrast signal.
+
+    For the Parker AIF the parameters of the functional form are copied from Table 1 of the reference
+
+    In the function ParkerAIF_refdataa() no delay was assumed. In this case the original AIF was used as a starting point (temp resol 4.97s, tot acquisition time 5 min);
+    This was extended with delays of exact multiplications of the temporal resolution (4.97s) as well as random seconds
+
+    References: Parker et al. MRM 2006, doi: 10.1002/mrm.21066
+
+    Returns
+    -------
+    pars : list of tuples
+        Input for pytest.mark.parametrize
+        Each tuple contains a set of parameters corresponding to 1 test case
+    """
+
+    # AIF parameters from Table 1 of Parker et al.
+    A1 = 0.809
+    sigma1 = 0.0563
+    T1 = 0.17046
+    A2 = 0.330
+    sigma2 = 0.132
+    T2 = 0.365
+    alpha = 1.05
+    beta = 0.1685
+    s = 38.078
+    tau = 0.483
+
+    label = []
+    time = []
+    cb_ref_values = []
+    delay = []
+
+    dt = 4.97  # temp resolution in seconds
+    time_current = np.arange(0, 5, dt/60)  # in min
+
+    # similar for a range of temporal resolutions
+    delay_range = np.array([0, dt, 2*dt, 5*dt, 2, 5, 10, 18, 31])  # indicated in seconds
+    for current_delay in delay_range:
+        current_label = 'delay_' + str(current_delay) + 's'
+        label.append(current_label)
+        gaussian1 = A1 / (sigma1 * np.sqrt(2 * np.pi)) * np.exp(-np.square(time_current - T1) / (2 * np.square(sigma1)))
+        gaussian2 = A2 / (sigma2 * np.sqrt(2 * np.pi)) * np.exp(-np.square(time_current - T2) / (2 * np.square(sigma2)))
+        modSigm = (alpha * np.exp(-beta * time_current)) / (1 + np.exp(-s * (time_current - tau)))
+        cb = np.add(gaussian1, gaussian2)
+        cb = np.add(cb, modSigm)
+        # shift cb according to delay value
+        n_shift = math.floor(current_delay/dt)
+        cb = np.pad(cb, (n_shift, 0), 'constant')
+        cb = cb[0:len(time_current)]  # keep array length constant
+        time.append(time_current)
+        cb_ref_values.append(cb)
+        delay.append(current_delay)  # assume no delay
+
+
+    # set the tolerance to use for this dataset
+    a_tol = [0.05] * len(label)
+    r_tol = [0.05] * len(label)
+
+    # convert to list of tuples (input for pytest.mark.parametrize)
+    pars = list(zip(label, time, cb_ref_values, delay, a_tol, r_tol))
 
     return pars
--- test/PopulationAIF_DCE/test_popAIF_Parker_ST_SydneyAus.py
+++ test/PopulationAIF_DCE/test_popAIF_Parker_ST_SydneyAus.py
@@ -2,22 +2,23 @@ import os
 import pytest
 import numpy as np
 from test.helpers import osipi_parametrize
-from src.original.PvH_NKI_NL.AIF.PopulationAIF import ParkerAIF
+from src.original.ST_USydAUS.InputFunctions import AIF, variableAIF, arr_shift
 import popAIF_data
 
 
 # All tests will use the same arguments and same data...
 arg_names = 'label, time, cb_ref_values, delay, r_tol, a_tol'
-test_data = popAIF_data.ParkerAIF_refdata()
+test_data = (
+        popAIF_data.ParkerAIF_refdata() +
+        popAIF_data.ParkerAIF_refdata_delay())
 
 # Use the test data to generate a parametrize decorator. This causes the following
 # test to be run for every test case listed in test_data...
 @osipi_parametrize(arg_names, test_data, xf_labels=[])
-def test_Parker_AIF_PvH_NKI_NL(label, time, cb_ref_values, delay, a_tol, r_tol):
+def test_Parker_AIF_ST_SydneyAus(label, time, cb_ref_values, delay, a_tol, r_tol):
 
     # prepare input data
-    # convert time to seconds
-    time = time*60
-    AIF_P = ParkerAIF(time)
-
+    time = time*60 # time array is expected in seconds
+    t0 = delay + time[1] # precontrast signal; t0 is expected to be in seconds; if no precontrast signal is expected, the value should be equal to the temp resolution
+    AIF_P = AIF(t0, time)
     np.testing.assert_allclose([AIF_P], [cb_ref_values], rtol=r_tol, atol=a_tol)
