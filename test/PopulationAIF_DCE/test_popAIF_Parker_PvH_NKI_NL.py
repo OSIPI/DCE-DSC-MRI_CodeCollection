63b40bbeaf9a28bf8d056d80d886828db5e6a71c
Petra van Houdt <p.v.houdt@nki.nl>
2021-10-07 09:55:27 +0200

    added test for ParkerAIF PvH_NKI_NL contribution

--- test/PopulationAIF_DCE/test_popAIF_Parker_PvH_NKI_NL.py
+++ test/PopulationAIF_DCE/test_popAIF_Parker_PvH_NKI_NL.py
@@ -2,22 +2,22 @@ import os
 import pytest
 import numpy as np
 from test.helpers import osipi_parametrize
-from src.original.PvH_NKI_NL.AIF.PopulationAIF import GeorgiouAIF
+from src.original.PvH_NKI_NL.AIF.PopulationAIF import ParkerAIF
 import popAIF_data
 
 
 # All tests will use the same arguments and same data...
 arg_names = 'label, time, cb_ref_values, r_tol, a_tol'
-test_data = popAIF_data.GeorgiouAIF_refdata()
+test_data = popAIF_data.ParkerAIF_refdata()
 
 # Use the test data to generate a parametrize decorator. This causes the following
 # test to be run for every test case listed in test_data...
 @osipi_parametrize(arg_names, test_data, xf_labels=[])
-def test_Georgiou_AIF_PvH_NKI_NL(label, time, cb_ref_values, a_tol, r_tol):
+def test_Parker_AIF_PvH_NKI_NL(label, time, cb_ref_values, a_tol, r_tol):
 
     # prepare input data
     # convert time to seconds
     time = time*60
-    AIF_G = GeorgiouAIF(time)
+    AIF_P = ParkerAIF(time)
 
-    np.testing.assert_allclose([AIF_G], [cb_ref_values], rtol=r_tol, atol=a_tol)
+    np.testing.assert_allclose([AIF_P], [cb_ref_values], rtol=r_tol, atol=a_tol)
