92d7adf9d81c29c78c6a6ed7db7c45dba913a666
Petra van Houdt <p.v.houdt@nki.nl>
2021-10-07 20:50:42 +0200

    Update setup.py
    added 'openpyxl' to see if this fixes the error in ci

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
         name='src',
         version = '1.0.0', 
-        install_requires = ['pytest','numpy','scipy','lmfit','pandas','joblib','matplotlib'],
+        install_requires = ['pytest','numpy','scipy','lmfit','pandas','joblib','matplotlib','openpyxl'],
         packages = find_packages()
         )
