93c27a6e829c2f88bfc14292693c012168e93425
Zaki A <zaki@live.ca>
2021-11-12 00:26:09 -0600

    Move utils to own module. Add wrappers modules

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
         name='osipi',
-        version = '1.0.0', 
-        install_requires = ['joblib', 'lmfit', 'mat73', 'matplotlib', 'numpy', 'pandas', 'pytest', 'scipy'],
+        version = '0.0.1', 
+        install_requires = ['imageio', 'joblib', 'lmfit', 'mat73', 'matplotlib', 'numpy', 'pandas', 'pytest', 'scipy'],
         package_dir={'osipi': 'src'},
         packages=[f'osipi.{module}' for module in find_packages('src')],
         )
--- src/wrappers/vfa.py
+++ src/wrappers/vfa.py
@@ -1,41 +1,61 @@
-import ..original.MJT_UoEdinburghUK.t1_fit as edinburgh
-import ..original.ST_USydAUS.VFAT1mapping as sydney
-import ..original.McGill_Can.vfa as mcgill
+import osipi.original.MJT_UoEdinburghUK.t1_fit as edinburgh
+import osipi.original.ST_USydAUS.VFAT1mapping as sydney
+import osipi.original.McGill_Can.vfa as mcgill
+from osipi.utils.nb import percenterror
 import matplotlib.pyplot as plt
+import numpy as np
 
-def vfa_fit(_signal, fa, tr, author, fittype = "linear", mask = None):
-    # Signal: numpy array, last dimension must be flip angle
+def vfa_fit(author, signal = None, fa = None, tr = None, fittype = "linear", mask = None, data = None):
+    # signal: numpy array, last dimension must be flip angle
     # fa: flip angles, in radian
     # tr: repetition time, in ms
+    # alternatively, `data` can be input containing the above three elements
+
+
+    # Unpack variables if data object is given
+    if not data == None:
+        signal = data.signal
+        fa = data.fa
+        tr = data.TR
     
+    # Some implementations only work if input signal is 1D
+    requires_1D = author in ["edinburgh", "sydney"]
+
+    # Reshape input as Voxels-by-FlipAngle matrix
     spatialdims = signal.shape[:-1]
-    signal = signal.reshape(-1, signal.shape[-1])
+    numvox = np.prod(spatialdims)
+    numangles = signal.shape[-1]
+    signal = signal.reshape(-1, numangles)
     if mask != None:
         signal = signal[mask[:] > 0, :]
-        
+
     if author == "edinburgh":
+        fa = np.rad2deg(fa)
         if fittype == "nonlinear":
-            _M0, _T1 = edinburgh.fit_vfa_nonlinear(signal, fa, tr)
-        else: # linear
-            _M0, _T1 = edinburgh.fit_vfa_linear(signal, fa, tr)
+            fitfunc = lambda S, FA, TR: edinburgh.fit_vfa_nonlinear(S, FA, TR)
+        else:
+            fitfunc = lambda S, FA, TR: edinburgh.fit_vfa_linear(S, FA, TR)
     elif author == "sydney":
         fa = np.rad2deg(fa)
-        tr = tr
-        numvox = np.prod(spatialdims)
-        _M0 = np.zeros(numvox)
-        _T1 = np.zeros(numvox)
-        for idx in range(numvox):
-            _M0[idx], _T1[idx] = sydney.VFAT1mapping(fa, signal[idx, :], tr, method = fittype)
+        fitfunc = lambda S, FA, TR: sydney.VFAT1mapping(FA, S, TR, method = fittype)
     elif author == "mcgill":
         if fittype == "nonlinear":
-            _M0, _T1 = mcgill.novifast(signal, fa, tr)
+           fitfunc = lambda S, FA, TR: mcgill.novifast(S, FA, TR)
         elif fittype == "nonlinear_noniterative":
-            _M0, _T1 = mcgill.novifast(signal, fa, tr, doiterative = False)
+            fitfunc = lambda S, FA, TR: mcgill.novifast(S, FA, TR, doiterative = False)
         else: # linear
-            _M0, _T1 = mcgill.despot(signal, fa, tr)
+            fitfunc = lambda S, FA, TR: mcgill.despot(S, FA, TR)
     else:
         print("ERROR: Unexpected author")
         return
+
+    if requires_1D:
+        _M0 = np.zeros(numvox)
+        _T1 = np.zeros(numvox)
+        for idx in range(numvox):
+            _M0[idx], _T1[idx] = fitfunc(signal[idx, :], fa, tr)
+    else:
+        _M0, _T1 = fitfunc(signal, fa, tr)
     
     if mask != None:
         M0 = np.zeros(spatialdims)
@@ -50,48 +70,69 @@ def vfa_fit(_signal, fa, tr, author, fittype = "linear", mask = None):
 def show_maps(fittedmaps = None, M0 = None, T1 = None, truth = None):
     if not np.any(M0):
         M0, T1 = fittedmaps
-        
+
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,8))
     ax1.imshow(M0)
     ax1.grid(False)
     plt.sca(ax1)
     plt.title("M0")
     if np.any(truth):
-      plt.xticks(np.arange(5, 150, 10), np.round((np.unique(truth.T1))).astype(int), rotation=45, ha="right")
-      plt.yticks(np.arange(5, 70, 10), np.round((np.unique(truth.M0))).astype(int))
+      xticklocs, yticklocs = get_ticklocs(M0)
+      plt.xticks(xticklocs, np.round((np.unique(truth.T1))).astype(int), rotation=45, ha="right")
+      plt.yticks(yticklocs, np.round((np.unique(truth.M0))).astype(int))
     
     ax2.imshow(T1)
     ax2.grid(False)
     plt.sca(ax2)
     plt.title("T1")
     if np.any(truth):
-      plt.xticks(np.arange(5, 150, 10), np.round((np.unique(truthdata.T1))).astype(int), rotation=45, ha="right")
-      plt.yticks(np.arange(5, 70, 10), np.round((np.unique(truthdata.M0))).astype(int))
+      xticklocs, yticklocs = get_ticklocs(M0)
+      plt.xticks(xticklocs, np.round((np.unique(truth.T1))).astype(int), rotation=45, ha="right")
+      plt.yticks(yticklocs, np.round((np.unique(truth.M0))).astype(int))
     
     fig.tight_layout()
-    return
+    return fig
 
-def show_error_maps(fits, title = "", showcbar = False, truth = None, clim = (-100, 100)):
+def show_error_maps(fits, idx = -1, title = "", showcbar = False, truth = None, clim = (-100, 100)):
     M0, T1 = fits
+
+    if (idx >= 0):
+        M0 = M0[:,:,idx]
+        T1 = T1[:,:,idx]
     
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,4))
     ax1.imshow(percenterror(M0, truth.M0), cmap = "PuOr_r", clim = clim)
     ax1.grid(False)
     plt.sca(ax1)
     plt.title("%error in M0")
-    plt.xticks(np.arange(5, 150, 10), np.round((np.unique(truthdata.T1))).astype(int), rotation=45, ha="right")
-    plt.yticks(np.arange(5, 70, 10), np.round((np.unique(truthdata.M0))).astype(int))
+    if np.any(truth):
+      xticklocs, yticklocs = get_ticklocs(M0)
+      plt.xticks(xticklocs, np.round((np.unique(truth.T1))).astype(int), rotation=45, ha="right")
+      plt.yticks(yticklocs, np.round((np.unique(truth.M0))).astype(int))
     
     im = ax2.imshow(percenterror(T1, truth.T1), cmap = "PuOr_r", clim = clim)
     ax2.grid(False)
     plt.sca(ax2)
     plt.title("%error in T1")
-    plt.xticks(np.arange(5, 150, 10), np.round((np.unique(truthdata.T1))).astype(int), rotation=45, ha="right")
-    plt.yticks(np.arange(5, 70, 10), np.round((np.unique(truthdata.M0))).astype(int))
+    if np.any(truth):
+      xticklocs, yticklocs = get_ticklocs(M0)
+      plt.xticks(xticklocs, np.round((np.unique(truth.T1))).astype(int), rotation=45, ha="right")
+      plt.yticks(yticklocs, np.round((np.unique(truth.M0))).astype(int))
     
     fig.suptitle(title)
     if showcbar:
         fig.colorbar(im, ax=[ax1,ax2], orientation='horizontal', shrink = 0.5)
     else:
         fig.tight_layout()
-    return
+    return fig
+
+def get_ticklocs(arr):
+    if arr.shape[0] > 20:
+        # Full resolution maps - 70x150
+        xticklocs = np.arange(5, 150, 10)
+        yticklocs = np.arange(5, 70, 10)
+    else:
+        # Downsampled maps - 7x15
+        xticklocs = np.arange(0, 15, 1)
+        yticklocs = np.arange(0, 7, 1)
+    return xticklocs, yticklocs
