aad091cb1619792d3bf1b5f0d787a9e90c921c0e
Zaki A <notZaki@users.noreply.github.com>
2021-11-18 21:53:45 -0600

    Re-arrange append_to_excel function
    Creates an empty file if it exists. Uses the `engine` keyword when reading/writing xlsx

--- test/DCEmodels/tools.py
+++ test/DCEmodels/tools.py
@@ -6,20 +6,18 @@ Created on Wed Nov  3 23:11:08 2021
 """
 import pandas as pd
 import os
+from openpyxl import Workbook
 
 def append_to_excel(data, columns):
     xlspath = r'test_results_ETM.xlsx'
-    with pd.ExcelWriter(xlspath) as writer:
-            
-        df = pd.DataFrame(data, index=None, columns=columns)
-        
-        df_source = None
-        if os.path.exists(xlspath):
-            df_source = pd.DataFrame(pd.read_excel(xlspath))
-        if df_source is not None:
-            df_dest = df_source.append(df)
-        else:
-            df_dest = df
-        
-        df_dest.to_excel(writer, index=False, columns=columns)
+    if not os.path.exists(xlspath):
+        wb = Workbook().save(filename=xlspath)
+    
+    df = pd.DataFrame(data, index=None, columns=columns)   
+    df_source = pd.DataFrame(pd.read_excel(xlspath, engine='openpyxl'))
+    if df_source is not None:
+        df_dest = df_source.append(df)
+    else:
+        df_dest = df
+    df_dest.to_excel(xlspath, engine='openpyxl', index=False, columns=columns)
     
