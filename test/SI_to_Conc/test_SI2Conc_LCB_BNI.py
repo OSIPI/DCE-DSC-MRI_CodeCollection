4f659ca6d13174ae530caf3d854eb612c1acc05f
LucyKershaw <lucyekershaw@gmail.com>
2021-11-23 16:01:18 +0000

    Corrected error in LCB_BNI.dce.signal_to_conc with permission. Reinstated test of this function

--- test/SI_to_Conc/test_SI2Conc_LCB_BNI.py
+++ test/SI_to_Conc/test_SI2Conc_LCB_BNI.py
@@ -18,7 +18,7 @@ def test_LCB_BNI_sig_to_conc(label, fa, tr, T1base, BLpts, r1, s_array, conc_arr
 
     ##Prepare input data
 
-    conc_curve = signal_to_conc(s_array, [1,BLpts], T1base, tr, fa, r1)
+    conc_curve = signal_to_conc(s_array, [1,BLpts-1], T1base, tr, fa, r1)
 
 
     np.testing.assert_allclose( conc_curve, conc_array, rtol=r_tol, atol=a_tol )
