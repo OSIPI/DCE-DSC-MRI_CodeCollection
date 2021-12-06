cc95cfca917de45ba3f4f73d0e349dfa232b6a9d
Zaki A <zaki@live.ca>
2021-12-05 20:38:31 -0600

    Remove incorrect dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
         name='osipi',
         version = '0.0.1', 
-        install_requires = ['imageio', 'joblib', 'lmfit', 'mat73', 'matplotlib', 'numpy', 'pandas', 'pytest', 'scipy', 'dicom', 'progressbar', 'cv2', 'opencv-python'],
+        install_requires = ['imageio', 'joblib', 'lmfit', 'mat73', 'matplotlib', 'numpy', 'pandas', 'pytest', 'scipy', 'dicom', 'progressbar', 'opencv-python'],
         package_dir={'osipi': 'src'},
         packages=[f'osipi.{module}' for module in find_packages('src')],
         )
