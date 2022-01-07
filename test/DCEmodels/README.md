fd6ffbbc9edf2572d09d3755e2fee5f99a289846
mjt320 <mjt320@googlemail.com>
2022-01-07 16:23:30 +0000

    Tidied up code + docstrings Updates/corrections as agreed with OGC Adjusted/simplified tolerances to almost match ISMRM abstract (tolerances were previously larger, probably due to incorrectly using delayed test data for code that doesn't fit delay) Split etofts + tofts tests into 2 separate files

--- test/DCEmodels/README.md
+++
@@ -1,5 +0,0 @@
-This folder will contain all the test files for DCE mapping. 
-
-Test data are selected voxels from:
-1: Prostate cancer patient from private collection
-2: TCIA RIDER NEURO MRI 1023805636
---
+++ test/DCEmodels/test_DCEmodels_tofts_MJT_UoEdinburghUK.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from ..helpers import osipi_parametrize
+from . import DCEmodels_data
+from src.original.MJT_UoEdinburghUK import dce_fit, pk_models, aifs
+
+arg_names = 'label, t_array, C_array, ca_array, ta_array, ve_ref, Ktrans_ref, arterial_delay_ref,  a_tol_ve, ' \
+            'r_tol_ve, a_tol_Ktrans,r_tol_Ktrans,a_tol_delay,r_tol_delay '
+test_data = (DCEmodels_data.dce_DRO_data_tofts())
+
+# Use the test data to generate a parametrize decorator. This causes the following
+# test to be run for every test case listed in test_data...
+@osipi_parametrize(arg_names, test_data, xf_labels=[])
+def test_MJT_UoEdinburghUK_tofts_model(label, t_array, C_array, ca_array, ta_array, ve_ref, Ktrans_ref,
+                                       arterial_delay_ref, a_tol_ve, r_tol_ve, a_tol_Ktrans, r_tol_Ktrans, a_tol_delay,
+                                       r_tol_delay):
+    # NOTES:
+
+    # prepare input data - create aif object
+    t_array = t_array  # /60  - in seconds
+    aif = aifs.patient_specific(t_array, ca_array)
+    pk_model = pk_models.tofts(t_array, aif)
+
+    # run test
+    pk_pars, C_t_fit = dce_fit.conc_to_pkp(C_array, pk_model)
+    Ktrans_meas = pk_pars['ktrans']
+    ve_meas = pk_pars['ve']
+    np.testing.assert_allclose([ve_meas], [ve_ref], rtol=r_tol_ve, atol=a_tol_ve)
+    np.testing.assert_allclose([Ktrans_meas], [Ktrans_ref], rtol=r_tol_Ktrans, atol=a_tol_Ktrans)
