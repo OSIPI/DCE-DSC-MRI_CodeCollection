9fcf1e51a65ae45f5d06c22cd2edf761bf942338
p.v.houdt <p.v.houdt@nki.nl>
2022-02-28 20:15:10 +0100

    change to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
 
-
 setup(
-        name='osipi_code_collection',
-        version = '0.0.2',
-        install_requires = ['dicom', 'imageio', 'joblib', 'lmfit', 
-                'mat73', 'matplotlib', 'numpy', 'opencv-python', 'openpyxl', 
-                'pandas', 'progressbar', 'pytest', 'scipy'],
-        include_package_data=True,
-        package_dir={'osipi_code_collection': 'src'},
-        packages=[f'osipi_code_collection.{module}' for module in find_packages('src')],
-        )
-        
+    name='osipi_code_collection',
+    version='0.0.2',
+    install_requires=['dicom', 'imageio', 'joblib', 'lmfit',
+                      'mat73', 'matplotlib', 'numpy', 'opencv-python', 'openpyxl',
+                      'pandas', 'progressbar', 'pytest', 'scipy'],
+    include_package_data=True,
+    package_dir={'osipi_code_collection': 'src'},
+    packages=[f'osipi_code_collection.{module}' for module in find_packages('src')],
+)
