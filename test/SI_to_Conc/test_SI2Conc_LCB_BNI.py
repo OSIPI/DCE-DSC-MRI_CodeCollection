b16f681082301a4b0770512a7f1f0f0285259b51
p.v.houdt <p.v.houdt@nki.nl>
2022-03-24 08:52:43 +0100

    fixed inconsistencies in log file name SI2Conc

--- test/SI_to_Conc/test_SI2Conc_LCB_BNI.py
+++ test/SI_to_Conc/test_SI2Conc_LCB_BNI.py
@@ -18,7 +18,7 @@ def setup_module(module):
     global filename_prefix # we want to change the global variable
     os.makedirs('./test/results/SI_to_Conc', exist_ok=True)
     filename_prefix = 'SI_to_Conc/TestResults_SI2Conc'
-    log_init(filename_prefix, '_LCB_BNI_sig_to_conc', ['label', 'time (us)', 'conc_curve', 'conc_array'])
+    log_init(filename_prefix, '_LCB_BNI_SI2Conc', ['label', 'time (us)', 'conc_curve', 'conc_array'])
 
 
 # Use the test data to generate a parametrize decorator. This causes the following
@@ -37,7 +37,7 @@ def test_LCB_BNI_sig_to_conc(label, fa, tr, T1base, BLpts, r1, s_array, conc_arr
     row_data = []
     for ref, meas in zip(conc_array, conc_curve):
         row_data.append([label, f"{exc_time:.0f}", ref, meas])
-    log_results(filename_prefix, '_LCB_BNI_sig_to_conc', row_data)
+    log_results(filename_prefix, '_LCB_BNI_SI2Conc', row_data)
 
     # testing
     np.testing.assert_allclose(conc_curve, conc_array, rtol=r_tol, atol=a_tol)
