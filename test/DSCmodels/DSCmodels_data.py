a55b83d81d37a34c795a5e6614c8803736f3b114
Jonathan Arvidsson <jonathan.arvidsson@vgregion.se>
2022-04-21 16:23:08 +0200

    testdata in .csv and better use of param decorator

--- test/DSCmodels/DSCmodels_data.py
+++ test/DSCmodels/DSCmodels_data.py
@@ -2,17 +2,12 @@ import os
 from xmlrpc.client import boolean
 
 import numpy as np
-#import pandas as pd
+import pandas as pd
 import scipy.io as sio
-import nibabel as nib
-
-def sig2conc(S,kappa,te,S0):
-    return -np.log(S/S0)/(kappa*te) # S = S0*np.exp(-kappa*C*TE)
 
 def dsc_DRO_data_vascular_model(delay=0):
     """
-    Import dsc signal data for testing.
-    Convert to concentration domain using provided simulation data S0, te, kappa.
+    Import dsc concentration data for testing.
 
     Data summary: digital reference object consisting of signal time curves 
     representing perfusion scenarios typical of grey and white matter.
@@ -22,8 +17,8 @@ def dsc_DRO_data_vascular_model(delay=0):
     of cerebral perfusion in dynamic susceptibility contrast MRI
 
     Similar digital reference objects have previously been used in publications 
-    evaluating novel perfusion deconvolution/estimation techniques including Wu et al. 2003 
-    (DOI 10.1002/mrm.10522), Mouridsen et al. 2006 (DOI 10.1016/j.neuroimage.2006.06.015 ), 
+    evaluating perfusion deconvolution/estimation techniques including Wu et al. 2003 
+    (DOI 10.1002/mrm.10522), Mouridsen et al. 2006 (DOI 10.1016/j.neuroimage.2006.06.015), 
     Chappell et al. 2015 (10.1002/mrm.25390)
     
     Parameters
@@ -41,75 +36,29 @@ def dsc_DRO_data_vascular_model(delay=0):
         Each tuple contains a set of parameters corresponding to 1 test case
 
     """
-    delay_aif = str(delay) #sim_cfg['delay_aif'][0][0][0]
-    # For later: make these input variables:
-    delay_level = 'high'
-    disperse_aif = '0'
-    disp_level = 'high'
 
-    # Read .mat file
-    datadir = os.path.join(os.path.dirname(__file__), 'data')
-    sim_cfg = sio.loadmat(datadir + '/sim_config' + '_delay_' + delay_aif  + '.mat')['sim_config']
-    cbf_g = sim_cfg['cbf_g'][0][0][0].tolist()
-    cbv_g = sim_cfg['cbv_gray'][0][0][0][0].tolist()
-    cbf_w = sim_cfg['cbf_w'][0][0][0].tolist()
-    cbv_w = sim_cfg['cbv_white'][0][0][0][0].tolist()
-    snr_t = sim_cfg['snr_t'][0][0][0][0].tolist()
-    image_size = sim_cfg['image_size'][0][0][0].tolist()
-    n_slices = sim_cfg['n_slices'][0][0][0][0].tolist()
-    n_time_points = sim_cfg['n_time_points'][0][0][0][0].tolist()
-    te = sim_cfg['te'][0][0][0][0].tolist() # Seconds
-    tr = sim_cfg['tr'][0][0][0][0].tolist() # Seconds
-    dk = sim_cfg['dk'][0][0][0]
-    residue_function = sim_cfg['residue_function'][0][0][0]
-    lamda = sim_cfg['lambda'][0][0][0][0].tolist()
-    S0 = sim_cfg['S0'][0][0][0][0].tolist()#100
-    C_gray = dict()
-    C_white = dict()
-    kappa_a = sio.loadmat(datadir + '/kappa_a.mat')['kappa_a'][0][0]
-    kappa_s = sio.loadmat(datadir + '/kappa_s.mat')['kappa_s'][0][0]
+    filename = os.path.join(os.path.dirname(__file__), 'data', 'dsc_data.csv')
+    # read from CSV to pandas
+    converters = {'C_tis': lambda x: np.fromstring(x, dtype=float, sep=' '),
+                  'C_aif': lambda x: np.fromstring(x, dtype=float, sep=' ')}
+    df = pd.read_csv(filename, converters=converters)
 
-    img = nib.load(datadir + '/aif.nii')
-    data = img.get_data() 
-    S_aif = np.squeeze(data)
-    C_aif = sig2conc(S_aif,kappa_a,te,S0)
-
-    for i, cbf_gray in enumerate(cbf_g):
-        label = '/St_disp_' + str(disperse_aif) +  '_' + dk + '_lvl_' + disp_level + '_delay_' + delay_aif + '_lvl_' + delay_level
-
-        if residue_function == 'gamma_dist':
-            label = label + '_resfunc_' + residue_function + '_lambda_' + str(lamda) + '_cbf_' + str(cbf_gray)
-        elif residue_function == 'linear':
-            label = label + '_resfunc_' + residue_function + '_lambda_' + '_cbf_' + str(cbf_gray)
-        else:
-            label = label + '_cbf_gw_' + str(cbf_gray) + '_cbf_wm_' + str(cbf_w[i])
-        
-        img = nib.load(datadir + label + '.nii')
-        data = img.get_data()
-        data = np.squeeze(data) # 4D -> 3D
-        S_data2D = data.reshape([data.shape[0]*data.shape[1], data.shape[2]],order =  'C')
-
-        # Convert to concentration 
-        C_data2D = sig2conc(S_data2D,kappa_s,te,S0)
-        cutRow = int(C_data2D.shape[0]/2-1)
-        C_gray[str(cbf_g[i])] = C_data2D[0:cutRow, :]
-        C_white[str(cbf_w[i])] = C_data2D[cutRow+1 : C_data2D.shape[0], :]
+    label = df['label'].tolist()  # label describing entry
+    C_tis = df['C_tis'].tolist()
+    C_aif = df['C_aif'].tolist()
+    cbv = df['cbv'].tolist()  
+    cbf = df['cbf'].tolist()
+    tr = df['tr'].tolist()
 
     # set the tolerance to use for this dataset
-    r_tol_cbv_g = [0.1]
-    r_tol_cbv_w = [0.1]
-    r_tol_cbf_g = [0.1] * len(cbf_g)
-    r_tol_cbf_w = [0.1] * len(cbf_w)
-
-    a_tol_cbv_g = [1]
-    a_tol_cbv_w = [1]
-    a_tol_cbf_g = [15] * len(cbf_g)
-    a_tol_cbf_w = [15] * len(cbf_w)
+    r_tol_cbv = [0.1] * len(tr)
+    r_tol_cbf = [0.1] * len(tr)
+    a_tol_cbv = [1] * len(tr)
+    a_tol_cbf = [15] * len(tr)
 
     # convert to list of tuples (input for pytest.mark.parametrize)
     pars = list(
-        zip(label, [C_gray], [C_white], [C_aif], [tr], [cbv_g], [cbv_w], [cbf_g], [cbf_w],
-            r_tol_cbv_g, r_tol_cbv_w, r_tol_cbf_g, r_tol_cbf_w, 
-            a_tol_cbv_g, a_tol_cbv_w, a_tol_cbf_g, a_tol_cbf_w))
+        zip(label, C_tis, C_aif, tr, cbv, cbf,
+            r_tol_cbv, r_tol_cbf,a_tol_cbv, a_tol_cbf))
 
     return pars
--- test/DSCmodels/test_DSCmodels_LcurveReg_SR_TBG_BNIPhoenixUSA.py
+++ test/DSCmodels/test_DSCmodels_LcurveReg_SR_TBG_BNIPhoenixUSA.py
@@ -1,50 +1,30 @@
 import os
 import numpy as np
-
+from time import perf_counter
 from ..helpers import osipi_parametrize
 from . import DSCmodels_data
 from src.original.SR_TBG_BNIPhoenixUSA.AIFDeconvolution.AIF_deconvolution import AIFdeconvolution
 from src.original.SR_TBG_BNIPhoenixUSA.DSCparameters.DSC_parameters import DSCparameters
 
 # All tests will use the same arguments and same data...
-arg_names = 'label, C_gray, C_white, C_aif, tr, cbv_g, cbv_w, cbf_g, cbf_w,' \
-            'r_tol_cbv_g, r_tol_cbv_w, r_tol_cbf_g, r_tol_cbf_w,' \
-            'a_tol_cbv_g, a_tol_cbv_w, a_tol_cbf_g, a_tol_cbf_w'
+arg_names = 'label, C_tis, C_aif, tr, cbv_ref, cbf_ref,' \
+            'r_tol_cbv, r_tol_cbf, a_tol_cbv, a_tol_cbf'
 
 test_data = DSCmodels_data.dsc_DRO_data_vascular_model()
 filename_prefix = ''
 
-
 # Use the test data to generate a parametrize decorator. This causes the following test to be run for every test case
 # listed in test_data...
 @osipi_parametrize(arg_names, test_data, xf_labels=[])
-def test_SR_TBG_BNIPhoenixUSA_LcurveReg(label, C_gray, C_white, C_aif, tr, cbv_g, cbv_w, cbf_g, cbf_w,
-                                        r_tol_cbv_g, r_tol_cbv_w, r_tol_cbf_g, r_tol_cbf_w,
-                                        a_tol_cbv_g, a_tol_cbv_w, a_tol_cbf_g, a_tol_cbf_w):
-    # run code
-    cbf_g_meas = [0] * len(cbf_g)
-    cbv_g_meas = [0] * len(cbf_g)
-    cbf_w_meas = [0] * len(cbf_w)
-    cbv_w_meas = [0] * len(cbf_w)
+def test_SR_TBG_BNIPhoenixUSA_LcurveReg(label, C_tis, C_aif, tr, cbv_ref, cbf_ref,
+                                        r_tol_cbv, r_tol_cbf, a_tol_cbv, a_tol_cbf):
 
-    # Estimate perfusion scenarios (only one curve per simulated scenario.)
-    for i in range(len(cbf_g)): 
-        C_tis = C_gray[str(cbf_g[i])][0,:]
-        residualFunction,_,_,_ = AIFdeconvolution(C_aif, C_tis, tr)
-        cbf_g_meas[i],cbv_g_meas[i],_= DSCparameters(C_tis, C_aif, residualFunction, tr)
-    
-    for i in range(len(cbf_w)): # Perfusion scenarios, only evaluating one curve per simulated scenario.    
-        C_tis = C_white[str(cbf_w[i])][0,:]
-        residualFunction,_,_,_ = AIFdeconvolution(C_aif, C_tis, tr)
-        cbf_w_meas[i],cbv_w_meas[i],_= DSCparameters(C_tis, C_aif, residualFunction, tr)
+    # Estimate perfusion scenario    
+    residualFunction,_,_,_ = AIFdeconvolution(C_aif, C_tis, tr)
+    cbf_meas,cbv_meas,_ = DSCparameters(C_tis, C_aif, residualFunction, tr)
 
-    # Run tests for each simulated perfusion scenario (cbv and cbf)
-    for i in range(len(cbf_g)):
-        np.testing.assert_allclose(cbv_g_meas[i], cbv_g*100, rtol=r_tol_cbv_g, atol=a_tol_cbv_g)
-        np.testing.assert_allclose(cbv_w_meas[i], cbv_w*100, rtol=r_tol_cbv_w, atol=a_tol_cbv_w)
-
-    for i in range(len(cbf_w)):
-        np.testing.assert_allclose(cbf_g_meas[i], cbf_g[i], rtol=r_tol_cbf_g, atol=a_tol_cbf_g)
-        np.testing.assert_allclose(cbf_w_meas[i], cbf_w[i], rtol=r_tol_cbf_w, atol=a_tol_cbf_w)
+    # test perfusion estimation    
+    np.testing.assert_allclose(cbv_meas, cbv_ref, rtol=r_tol_cbv, atol=a_tol_cbv)
+    np.testing.assert_allclose(cbf_meas, cbf_ref, rtol=r_tol_cbf, atol=a_tol_cbf)
 
 
