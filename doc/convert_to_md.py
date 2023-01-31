f3057a1ed465d8cbafa2decf24296d5707535c35
Zaki A <zaki@live.ca>
2023-01-31 15:47:19 -0500

    Use markdown table in jupyterbook

--- doc/convert_to_md.py
+++ doc/convert_to_md.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('doc/code_contributions_record.csv')
-with open('doc/code_contributions_record.md', 'w') as md:
-    df.to_markdown(buf=md, index=False)
+md = df.to_markdown(index=False).replace("(../", "(https://github.com/OSIPI/DCE-DSC-MRI_CodeCollection")
+with open('notebook/contributors.md', 'w') as f:
+    f.write("# Overview of code collection\n\n")
+    f.write(md)
