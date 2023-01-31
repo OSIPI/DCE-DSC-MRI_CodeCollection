52bdb16540c7866c040a4fb15abe4b1b8f64f117
Zaki A <zaki@live.ca>
2023-01-31 16:24:31 -0500

    Fix typo

--- doc/convert_to_md.py
+++ doc/convert_to_md.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('doc/code_contributions_record.csv')
-md = df.to_markdown(index=False).replace("(../", "(https://github.com/OSIPI/DCE-DSC-MRI_CodeCollection")
-with open('notebook/overview_of_code_collection.md', 'w') as f:
+md = df.to_markdown(index=False).replace("(../", "(https://github.com/OSIPI/DCE-DSC-MRI_CodeCollection/tree/develop/")
+with open('notebooks/overview_of_code_collection.md', 'w') as f:
     f.write("# Overview of code collection\n\n")
     f.write(md)
