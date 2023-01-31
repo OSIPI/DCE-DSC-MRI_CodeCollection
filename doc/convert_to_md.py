f0b6262ef9e47900ebcd8d9f02a456cf071a5822
Zaki A <zaki@live.ca>
2023-01-31 16:17:57 -0500

    Rename the markdown file to match header

--- doc/convert_to_md.py
+++ doc/convert_to_md.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('doc/code_contributions_record.csv')
 md = df.to_markdown(index=False).replace("(../", "(https://github.com/OSIPI/DCE-DSC-MRI_CodeCollection")
-with open('notebook/contributors.md', 'w') as f:
+with open('notebook/overview_of_code_collection.md', 'w') as f:
     f.write("# Overview of code collection\n\n")
     f.write(md)
