01bc8ec8a368008c4f3487b6b83f83e414a51f1a
Luis Torres <luistorres@flywheel.io>
2023-04-13 11:52:22 -0500

    add back setup.py for noow.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='osipi_code_collection',
+    version='0.0.2',
+    install_requires=['dicom==0.9.9.post1', 'imageio==2.19.3', 'joblib==1.1.0', 'lmfit==1.0.3',
+                      'mat73==0.59', 'matplotlib==3.5.2', 'nibabel==3.2.2', 'numpy==1.23.0rc2',
+                      'opencv-python==4.5.5.64', 'openpyxl==3.2.0b1', 'pandas==1.4.2',
+                      'progressbar==2.5', 'pytest==7.1.2', 'scipy==1.8.1'],
+    include_package_data=True,
+    package_dir={'osipi_code_collection': 'src'},
+    packages=[f'osipi_code_collection.{module}' for module in find_packages('src')],
+)
